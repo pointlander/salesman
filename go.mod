c8d57745abc70002171e13d1c23c89e8d24bf967
Andrew Snodgrass <git@pointlander.info>
2022-04-18 22:32:19 -0500

    Added eigen vectors

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/pointlander/salesman
 
 go 1.17
 
-require github.com/pointlander/pagerank v0.0.0-20210619221740-830548a59275 // indirect
+require (
+	github.com/pointlander/pagerank v0.0.0-20210619221740-830548a59275 // indirect
+	gonum.org/v1/gonum v0.11.0 // indirect
+)
--- main.go
+++ main.go
@@ -7,10 +7,18 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/cmplx"
 	"math/rand"
 	"sort"
 
+	"gonum.org/v1/gonum/mat"
+
 	"github.com/pointlander/pagerank"
+)
+
+const (
+	// Size is the size of the matrix
+	Size = 4
 )
 
 func main() {
@@ -21,15 +29,15 @@ func main() {
 		42, 30, 0, 12,
 		35, 34, 12, 0,
 	}
-	for i := 0; i < 4; i++ {
-		for j := i + 1; j < 4; j++ {
+	for i := 0; i < Size; i++ {
+		for j := i + 1; j < Size; j++ {
 			value := float64(rand.Intn(8) + 1)
 			a[i*4+j] = value
 			a[j*4+i] = value
 		}
 	}
-	for i := 0; i < 4; i++ {
-		for j := 0; j < 4; j++ {
+	for i := 0; i < Size; i++ {
+		for j := 0; j < Size; j++ {
 			fmt.Printf("%f ", a[i*4+j])
 		}
 		fmt.Printf("\n")
@@ -44,19 +52,19 @@ func main() {
 				continue
 			}
 			skipped = false
-			value, x := search(sum+a[i*4+j], j, append(nodes, j), visited)
+			value, x := search(sum+a[i*Size+j], j, append(nodes, j), visited)
 			if value < smallest {
 				smallest, cities = value, x
 			}
 		}
 		if skipped {
-			return sum + a[i*4+nodes[0]], append(cities, nodes[0])
+			return sum + a[i*Size+nodes[0]], append(cities, nodes[0])
 		}
 		return smallest, cities
 	}
-	sum, nodes := search(0, 0, []int{0}, [4]bool{})
+	sum, nodes := search(0, 0, []int{0}, [Size]bool{})
 	for i := 1; i < 4; i++ {
-		s, n := search(0, i, []int{i}, [4]bool{})
+		s, n := search(0, i, []int{i}, [Size]bool{})
 		if s < sum {
 			sum, nodes = s, n
 		}
@@ -69,7 +77,7 @@ func main() {
 			if i == j {
 				continue
 			}
-			graph.Link(uint64(i), uint64(j), a[i*4+j])
+			graph.Link(uint64(i), uint64(j), a[i*Size+j])
 		}
 	}
 	type City struct {
@@ -95,8 +103,31 @@ func main() {
 	total := 0.0
 	last := pageNodes[0]
 	for _, node := range pageNodes[1:] {
-		total += a[last*4+node]
+		total += a[last*Size+node]
 		last = node
 	}
 	fmt.Println(total, pageNodes)
+
+	adjacency := mat.NewDense(Size, Size, a)
+	var eig mat.Eigen
+	ok := eig.Factorize(adjacency, mat.EigenRight)
+	if !ok {
+		panic("Eigendecomposition failed")
+	}
+
+	values := eig.Values(nil)
+	for i, value := range values {
+		fmt.Println(i, value, cmplx.Abs(value), cmplx.Phase(value))
+	}
+	fmt.Printf("\n")
+
+	vectors := mat.CDense{}
+	eig.VectorsTo(&vectors)
+	for i := 0; i < Size; i++ {
+		for j := 0; j < Size; j++ {
+			fmt.Printf("%f ", vectors.At(i, j))
+		}
+		fmt.Printf("\n")
+	}
+	fmt.Printf("\n")
 }
