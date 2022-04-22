888a206b1a85a9f402b05de98ec55ec5dbf5c804
Andrew Snodgrass <git@pointlander.info>
2022-04-21 21:54:52 -0500

    Added neural network

--- go.mod
+++ go.mod
@@ -9,9 +9,13 @@ require (
 	github.com/go-latex/latex v0.0.0-20210823091927-c0d11ff05a81 // indirect
 	github.com/go-pdf/fpdf v0.6.0 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/pointlander/gradient v0.0.0-20201206051041-dbff480e6d28 // indirect
 	github.com/pointlander/pagerank v0.0.0-20210619221740-830548a59275 // indirect
+	github.com/ziutek/blas v0.0.0-20190227122918-da4ca23e90bb // indirect
 	golang.org/x/image v0.0.0-20220302094943-723b81ca9867 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gonum.org/v1/gonum v0.11.0 // indirect
 	gonum.org/v1/plot v0.11.0 // indirect
+	google.golang.org/protobuf v1.24.0 // indirect
 )
--- main.go
+++ main.go
@@ -20,6 +20,7 @@ import (
 	"gonum.org/v1/plot/vg"
 	"gonum.org/v1/plot/vg/draw"
 
+	"github.com/pointlander/gradient/tf64"
 	"github.com/pointlander/pagerank"
 )
 
@@ -36,6 +37,10 @@ var (
 func main() {
 	flag.Parse()
 	rand.Seed(1)
+	if *FlagDebug {
+		test()
+		return
+	}
 	count := 0
 	for i := 0; i < 1024; i++ {
 		if test() {
@@ -218,6 +223,157 @@ func Eigen(a []float64) (*mat.CDense, float64, []int) {
 	return &vectors, minTotal, minLoop
 }
 
+// Neural uses a neural network to solve the traveling salesman problem
+func Neural(a []float64) (float64, []int) {
+	Scale := 4
+	set := tf64.NewSet()
+	set.Add("A", Size, Size)
+	set.Add("X", Size, Scale*Size)
+	set.Add("B", Size)
+
+	w := set.Weights[0]
+	for i := 0; i < Size*Size; i++ {
+		w.X = append(w.X, a[i])
+	}
+
+	w = set.Weights[1]
+	factor := math.Sqrt(2.0 / float64(w.S[0]))
+	for i := 0; i < cap(w.X); i++ {
+		w.X = append(w.X, rand.NormFloat64()*factor)
+	}
+
+	set.Weights[2].X = set.Weights[2].X[:cap(set.Weights[2].X)]
+
+	deltas := make([][]float64, 0, 8)
+	for _, p := range set.Weights {
+		deltas = append(deltas, make([]float64, len(p.X)))
+	}
+
+	l1 := tf64.Sigmoid(tf64.Add(tf64.Mul(set.Get("A"), set.Get("X")), set.Get("B")))
+	cost := tf64.Avg(tf64.Quadratic(l1, set.Get("X")))
+
+	alpha, eta, iterations := .3, .3, 1024
+	points := make(plotter.XYs, 0, iterations)
+	i := 0
+	for i < iterations {
+		total := 0.0
+		set.Zero()
+
+		total += tf64.Gradient(cost).X[0]
+		sum := 0.0
+		for _, p := range set.Weights[1:] {
+			for _, d := range p.D {
+				sum += d * d
+			}
+		}
+		norm := math.Sqrt(sum)
+		scaling := 1.0
+		if norm > 1 {
+			scaling = 1 / norm
+		}
+
+		for j, w := range set.Weights[1:] {
+			for k, d := range w.D {
+				deltas[j+1][k] = alpha*deltas[j+1][k] - eta*d*scaling
+				set.Weights[j+1].X[k] += deltas[j+1][k]
+			}
+		}
+
+		points = append(points, plotter.XY{X: float64(i), Y: total})
+		if *FlagDebug {
+			fmt.Println(i, total)
+		}
+		if total < .01 {
+			break
+		}
+		i++
+	}
+
+	if *FlagDebug {
+		p := plot.New()
+
+		p.Title.Text = "epochs vs cost"
+		p.X.Label.Text = "epochs"
+		p.Y.Label.Text = "cost"
+
+		scatter, err := plotter.NewScatter(points)
+		if err != nil {
+			panic(err)
+		}
+		scatter.GlyphStyle.Radius = vg.Length(1)
+		scatter.GlyphStyle.Shape = draw.CircleGlyph{}
+		p.Add(scatter)
+
+		err = p.Save(8*vg.Inch, 8*vg.Inch, "cost.png")
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	distances := make([]float64, Size*Size)
+	for i := 0; i < Size; i++ {
+		for j := 0; j < Size; j++ {
+			if i == j {
+				continue
+			}
+			sum := 0.0
+			for k := 0; k < Scale*Size; k++ {
+				x := w.X[i+k*Size] - w.X[j+k*Size]
+				sum += x * x
+			}
+			distances[i*Size+j] = math.Sqrt(sum)
+		}
+	}
+	if *FlagDebug {
+		for i := 0; i < Size; i++ {
+			for j := 0; j < Size; j++ {
+				fmt.Printf("%f ", distances[i*Size+j])
+			}
+			fmt.Printf("\n")
+		}
+	}
+	minTotal, minLoop := math.MaxFloat64, make([]int, 0, 8)
+	for offset := 0; offset < Size; offset++ {
+		visited := [Size]bool{}
+		state := offset
+		visited[state] = true
+		total, loop := 0.0, make([]int, 0, 8)
+		loop = append(loop, state)
+		for i := 0; i < Size; i++ {
+			min, k := math.MaxFloat64, 0
+			done := true
+			for j := 0; j < Size; j++ {
+				if j == state || visited[j] {
+					continue
+				}
+				done = false
+				if v := distances[state*Size+j]; v < min {
+					min, k = v, j
+				}
+			}
+			if done {
+				loop = append(loop, loop[0])
+				break
+			}
+			state = k
+			visited[state] = true
+			loop = append(loop, state)
+		}
+		last := loop[0]
+		for _, node := range loop[1:] {
+			total += a[last*Size+node]
+			last = node
+		}
+		if total < minTotal && loop[0] == loop[Size] {
+			minTotal, minLoop = total, loop
+		}
+	}
+	if *FlagDebug {
+		fmt.Println(minTotal, minLoop)
+	}
+	return minTotal, minLoop
+}
+
 func test() bool {
 	a := []float64{
 		0, 20, 42, 35,
@@ -247,7 +403,8 @@ func test() bool {
 	sum, nodes := Search(a)
 	total, pageNodes := PageRank(a)
 	vectors, minTotal, minLoop := Eigen(a)
-	_, _, _, _ = nodes, total, pageNodes, minLoop
+	//neuralTotal, neuralLoop := Neural(a)
+	_, _, _, _, _ = nodes, total, pageNodes, minLoop, minTotal
 
 	ranks := mat.NewDense(Size, Size, nil)
 	for i := 0; i < Size; i++ {
